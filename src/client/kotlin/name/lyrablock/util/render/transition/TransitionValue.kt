8c62f3e8b22783629eb7c87de91d6e5934313254
TheColdPot <guohw979@outlook.com>
2025-06-03 23:19:37 +0800

    feat: add TransitionValue for ui

---
+++ src/client/kotlin/name/lyrablock/util/render/transition/TransitionValue.kt
@@ -0,0 +1,46 @@
+package name.lyrablock.util.render.transition
+
+import kotlinx.datetime.Clock
+
+// A value that transitions over time.
+class TransitionValue<T: TransitionValue.Transitionable<T>>(
+    defaultValue: T,
+    val timeFunction: TimeFunction = TimeFunction.LINEAR
+) {
+    // A transitionable value must be able to be added and subtracted with the same type,
+    // and also able to be divided by a Double to scale it.
+    interface Transitionable<T> {
+        operator fun plus(that: T): T
+        operator fun minus(that: T): T
+        operator fun div(that: Double): T
+    }
+
+    var startValue: T = defaultValue
+    var targetValue: T = defaultValue
+    var startTime: Long = 0L
+    var transitionTime: Int = 0
+
+    /**
+     * Starts a transition to the target value over the specified time.
+     *
+     * If it is started during an existing transition, it interrupts the existing one.
+     *
+     * @param target The target value to transition to.
+     * @param time The time in milliseconds over which to transition.
+     */
+    fun startTransition(target: T, time: Int) {
+        startValue = currentValue()
+        targetValue = target
+        startTime = Clock.System.now().toEpochMilliseconds()
+        transitionTime = time
+    }
+
+    fun currentValue(): T {
+        val elapsed = Clock.System.now().toEpochMilliseconds() - startTime
+
+        if (elapsed > startTime) return targetValue
+
+        val progress = timeFunction.invoke(elapsed.toDouble())
+        return startValue + (targetValue - startValue) / progress
+    }
+}
