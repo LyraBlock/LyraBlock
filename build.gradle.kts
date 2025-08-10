1d3c480c569d29132e65a76a6d47296a123fbacf
TheColdPot <guohw979@outlook.com>
2025-08-10 20:04:36 +0800

    chore(build): use .kts for gradle build

---
+++ build.gradle.kts
@@ -0,0 +1,85 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    id("fabric-loom") version "1.10-SNAPSHOT"
+    id("maven-publish")
+    id("org.jetbrains.kotlin.jvm") version "2.1.20"
+}
+
+version = project.property("mod_version")!!
+group = project.property("maven_group")!!
+
+base {
+    archivesName.set(project.property("archives_base_name") as String)
+}
+
+repositories {
+    // Add repositories to retrieve artifacts from in here.
+    // Only use this when depending on other mods, because Loom adds essential
+    // repositories for Minecraft and libraries automatically.
+}
+
+loom {
+    splitEnvironmentSourceSets()
+
+    mods {
+        create("lyrablock") {
+            // sourceSet(sourceSets.main)
+            sourceSet(sourceSets["client"])
+        }
+    }
+}
+
+dependencies {
+    minecraft("com.mojang:minecraft:${project.property("minecraft_version")}")
+    mappings("net.fabricmc:yarn:${project.property("yarn_mappings")}:v2")
+    modImplementation("net.fabricmc:fabric-loader:${project.property("loader_version")}")
+
+    modImplementation("net.fabricmc.fabric-api:fabric-api:${project.property("fabric_version")}")
+    modImplementation("net.fabricmc:fabric-language-kotlin:${project.property("fabric_kotlin_version")}")
+}
+
+tasks.processResources {
+    inputs.property("version", project.version)
+
+    filesMatching("fabric.mod.json") {
+        expand(mapOf("version" to inputs.properties["version"]))
+    }
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.release.set(21)
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_21)
+    }
+}
+
+java {
+    withSourcesJar()
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+}
+
+tasks.jar {
+    inputs.property("archivesName", base.archivesName.get())
+
+    from("LICENSE") {
+        rename { "${it}_${inputs.properties["archivesName"]}" }
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            artifactId = project.property("archives_base_name") as String
+            from(components["java"])
+        }
+    }
+
+    repositories {
+        // Add repositories for publishing here
+    }
+}
