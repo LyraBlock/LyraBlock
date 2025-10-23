50a1f172c8112fb123f7c97799f4df58c1687e31
TheColdPot <guohw979@outlook.com>
2025-10-23 14:53:41 +0800

    refactor: use for instead of forEach for clarity

--- src/client/kotlin/app/lyrablock/lyra/LyraModule.kt
+++ src/client/kotlin/app/lyrablock/lyra/LyraModule.kt
@@ -10,22 +10,21 @@ annotation class LyraModule() {
             val reflections = Reflections(packageName)
             val annotated = reflections.getTypesAnnotatedWith(LyraModule::class.java)
 
-            annotated.forEach {
-                if (it.isAnnotationPresent(ByMixin::class.java)) {
-                    // Works as `continue`.
-                    return@forEach
-                }
+            for (target in annotated) {
+                if (target.isAnnotationPresent(ByMixin::class.java))
+                    continue
+
                 try {
-                    val kClass = it.kotlin
+                    val kClass = target.kotlin
                     // Here calls `init`.
                     val obj = kClass.objectInstance
                     if (obj == null) {
-                        LyraBlockClient.LOGGER.warn("Found illegal module ${it.name}.")
-                        return@forEach
+                        LyraBlockClient.LOGGER.warn("Found illegal module ${target.name}.")
+                        continue
                     }
-                    LyraBlockClient.LOGGER.debug("Loaded ${it.name}.")
+                    LyraBlockClient.LOGGER.debug("Loaded ${target.name}.")
                 } catch (error: Throwable) {
-                    LyraBlockClient.LOGGER.error("Failed to load ${it.name}!", error)
+                    LyraBlockClient.LOGGER.error("Failed to load ${target.name}!", error)
                 }
             }
         }
