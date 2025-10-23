48a8242425f78eeecb261125cd22b4d1fcedf711
TheColdPot <guohw979@outlook.com>
2025-10-23 13:50:49 +0800

    feat: experimentally add waypoint helper

--- src/client/kotlin/app/lyrablock/lyra/feature/misc/WaypointMarker.kt
+++ src/client/kotlin/app/lyrablock/lyra/feature/misc/WaypointMarker.kt
@@ -3,7 +3,9 @@ package app.lyrablock.lyra.feature.misc
 import app.lyrablock.lyra.LyraModule
 import net.fabricmc.fabric.api.client.message.v1.ClientReceiveMessageEvents
 import net.minecraft.text.ClickEvent
+import net.minecraft.text.HoverEvent
 import net.minecraft.text.Text
+import net.minecraft.util.Formatting
 
 @LyraModule
 object WaypointMarker {
@@ -11,28 +13,42 @@ object WaypointMarker {
         ClientReceiveMessageEvents.MODIFY_GAME.register(::modifyGameMessage)
     }
 
-    val coordinatesRegex = Regex("""x:\s*(\d+(?:\.\d+)?)\s*,\s*y:\s*(\d+(?:\.\d+)?)\s*,\s*z:\s*(\d+(?:\.\d+)?)|\s*(\d+(?:\.\d+)?)[,\s]\s*(\d+(?:\.\d+)?)[,\s]\s*(\d+(?:\.\d+)?)""")
+    // Matches `x: 1.0, y: 2.3, z: 3.2``.
+    val namedCoordinatesRegex =
+        Regex("""x:\s*(?<x>\d+(?:\.\d+)?)\s*,\s*y:\s*(?<y>\d+(?:\.\d+)?)\s*,\s*z:\s*(?<z>\d+(?:\.\d+)?)""")
+
+    // Matches `1.0 2.3 3.2` or `1.0, 2.3, 3.2`, but not `1.0, 2.3, 3.2, 4.5`
+    val separatedCoordinatesRegex =
+        Regex("""\s*(?<x>\d+(?:\.\d+)?)[,\s]\s*(?<y>\d+(?:\.\d+)?)[,\s]\s*(?<z>\d+(?:\.\d+)?)(?![,\s]\s+\d)""")
 
     fun modifyGameMessage(message: Text, overlay: Boolean): Text {
         if (overlay) return message
         val messageString = message.string
-        if (!coordinatesRegex.containsMatchIn(messageString)) return message
-//        if (ChatUtils.toHypixel(messageString) == null) return message
-        val modifiedMessage = Text.empty()
-        val match = coordinatesRegex.find(messageString) ?: return message
+        val match = namedCoordinatesRegex.find(messageString)
+            ?: separatedCoordinatesRegex.find(messageString)
+            ?: return message
         val range = match.range
+
+        // Construct a message
+        val modifiedMessage = Text.empty()
         modifiedMessage.append(messageString.take(range.first))
-        val component = Text.literal(match.value)
+        val clickableComponent = Text.literal(match.value)
 
-        val style = component.style.withUnderline(true).withClickEvent(ClickEvent.SuggestCommand("/TODO waypoint add " + match.let {
-            val x = it.groups[1]?.value ?: it.groups[4]?.value
-            val y = it.groups[2]?.value ?: it.groups[5]?.value
-            val z = it.groups[3]?.value ?: it.groups[6]?.value
+        val style = clickableComponent.style
+            .withUnderline(true)
+            .withClickEvent(ClickEvent.SuggestCommand("/lyra:todo waypoint add " + match.let {
+                val x = it.groups["x"]!!.value
+                val y = it.groups["y"]!!.value
+                val z = it.groups["z"]!!.value
 
-            "$x $y $z"
-        }))
-        component.style = style
-        modifiedMessage.append(component)
+                "$x $y $z"
+            }))
+            .withHoverEvent(HoverEvent.ShowText(
+                Text.translatable("lyra.waypoint.add_tooltip").formatted(Formatting.YELLOW)
+            ))
+            .withColor(Formatting.GOLD)
+        clickableComponent.style = style
+        modifiedMessage.append(clickableComponent)
         modifiedMessage.append(messageString.substring(range.last + 1))
 
         return modifiedMessage
